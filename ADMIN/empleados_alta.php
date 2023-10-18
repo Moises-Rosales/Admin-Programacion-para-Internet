aa5091dc924f908b6cbc595d10082e70a5756bd5
Moises-Rosales <46798343+Moises-Rosales@users.noreply.github.com>
2023-10-18 08:18:44 -0600

    Add files via upload

--- ADMIN/empleados_alta.php
+++ ADMIN/empleados_alta.php
@@ -8,6 +8,7 @@
 
     <a href="empleados_lista.php">Regresar al Listado</a><br><br>
 
+    <script src="js/jquery-3.3.1.min.js"></script>
     <script>
         function validarCampos() {
             
@@ -29,6 +30,15 @@
                 setTimeout("$('#mensaje').html('');",5000)
             }
         }
+        function entra(){
+            $('#mensaje').html('Entraste al campo');
+                setTimeout("$('#mensaje').html('');",5000)
+        }
+        function sale(){
+            $('#correo')
+            $('#mensajeCorreo').html('El correo ya Existe');
+                setTimeout("$('#mensaje').html('');",5000)
+        }
     </script>
 </head>
 
@@ -36,7 +46,7 @@
     <form name="Alta" id="Alta">
         <input type="text" name="nombre" id="nombre" placeholder="Nombre" /> <br>
         <input type="text" name="apellidos" id="apellidos" placeholder="Apellidos" /> <br>
-        <input type="text" name="correo" id="correo" placeholder="Correo" /> <br>
+        <input onfocus="entra();" onblur="sale();" type="text" name="correo" id="correo" placeholder="Correo" /> <br>
         <input type="text" name="pass" id="pass" placeholder="Escribe tu password" /><br>
         <select name="rol" id="rol"> <br>
             <option value="0">Selecciona</option>
@@ -44,6 +54,7 @@
             <option value="2">Ejecutivo</option>
         </select><br>
         <input type="submit" value="Salvar" onclick="validarCampos();return false;" />
+        <div id=Mensaje></div>
 
 
     </form>
